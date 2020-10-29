785d6d1c94e51321058265508386557d6d28789b
Tarik Sabin <tarik.sabin@gmail.com>
2020-10-29 15:32:10 +0000

    Main camera and building camera switch, also pauses when in build mode.

--- Assets/YourProjectName/Scripts/Building.cs
+++ Assets/YourProjectName/Scripts/Building.cs
@@ -7,12 +7,44 @@ public class Building : MonoBehaviour
     public int width;
     public int height;
     public int tileSize;
+    public Camera mainCamera;
+    public Camera buildCamera; 
     public List<GameObject> Buildings = new List<GameObject>();
 
 
     void Start()
     {
         BuildGrid BuildGrid = new BuildGrid(width, height, tileSize, Buildings);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            CameraChange();
+        }
+    }
+
+    private void CameraChange()
+    {
+        switch(mainCamera.enabled)
+        {
+            case true:
+                mainCamera.enabled = false;
+                buildCamera.enabled = true;
+                Time.timeScale = 0;
+                break;
+            case false:
+                mainCamera.enabled = true;
+                buildCamera.enabled = false;
+                Time.timeScale = 1;
+                break;
+            default:
+                mainCamera.enabled = false;
+                buildCamera.enabled = true;
+                Time.timeScale = 0;
+                break;
+        }
     }
 
 }
