4372e84d1b76f1406052a6a5f92b55c9e0153553
Tarik Sabin <tarik.sabin@gmail.com>
2020-10-29 14:39:11 +0000

    Now spawn in correct position, next make tiles selectable.

--- Assets/YourProjectName/Scripts/BuildGrid.cs
+++ Assets/YourProjectName/Scripts/BuildGrid.cs
@@ -15,28 +15,29 @@ public class BuildGrid
         this.Buildings = Buildings;
         this.width = width;
         this.height = height;
+        this.tileSize = tileSize;
         tileArray = new int[width, height];
 
         for (int x = 0; x < tileArray.GetLength(0); x++)
         {
             for (int y = 0; y < tileArray.GetLength(1); y++)
             {
-                
+                tileArray[x, y] = 0;
                 Object.Instantiate(Buildings[0], new Vector3(TranslateToWorldPos(x, y)[0], 0f, TranslateToWorldPos(x, y)[1]), Quaternion.identity);
             }
         }
     }
 
         
-
+    // Get the world position to set tile on.
     private float[] TranslateToWorldPos(int x, int y)
     {
         float[] xzPos = new float[2];
         Vector3 worldPos = new Vector3(x, y) * tileSize;
 
-        // Sets for right axes
-        worldPos.x -= 0.5f * tileSize;
-        worldPos.z = worldPos.y - 0.5f * tileSize;
+        // Sets for right axes and positions centrally.
+        worldPos.x -= 0.5f * tileSize * (width - 1);
+        worldPos.z = worldPos.y - 0.5f * tileSize * (height - 1);
         worldPos.y = 0;
 
         xzPos[0] = worldPos.x;
