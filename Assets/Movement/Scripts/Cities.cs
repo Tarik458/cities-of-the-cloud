2e9be54b50b401c7b5a7af6c2892f1d3378813c2
Robomoo <robomooer@gmail.com>
2020-11-25 01:12:29 +0000

    Fixed a lot of stuff, see description
    -Reorganised file structure: Everything relating to building system Tarik and I are working onis in 'BuildSystem'. Ludo's stuff is in 'Movement', everything else is in 'General'
    -Added LeanPool plugin
    
    Build system:
    -Changed the name of the main class from 'Building' to 'BuildingSystem'
    -Added a system for keeping track of which building is in which tile
    -Added a system that checks if the building being placed can be afforded in resources. Haven't added any resource values or tested yet.
    -Added a new struct (BuildingSystem.Building) that stores of a building's info, e.g. name, cost, description, model etc.
    -Added a hovering marker when the player is trying to place a building
    -Fixed the 'TestingMove' scene so that it works now
    
    Todo next:
    -Tidy up BuildingSystem class
    -Add a UI resource display
    -Show description of selected building
    -Change placement system so that multiple buildings can be placed without needing to click the same button multiple times

---
+++ Assets/Movement/Scripts/Cities.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cities : MonoBehaviour
+{
+    public int InWindZones;
+    public Rigidbody rb;
+    public GameObject WindZone;
+
+    public float forwardForce;
+    public float sideForce;
+
+    public float citySpeed;
+
+    bool m_movementPaused = false;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "WindArea")
+        {
+            WindZone = coll.gameObject;
+            InWindZones++;
+        }
+    }
+    void OnTriggerExit(Collider coll)
+    {
+
+        if (coll.gameObject.tag == "WindArea")
+        {
+            InWindZones--;
+        }
+
+    }
+
+    void Update()
+    {
+        if (!m_movementPaused)
+        {
+            if (Input.GetKey("a"))
+            {
+                rb.AddForce(-sideForce, 0, 0);
+            }
+
+            if (Input.GetKey("d"))
+            {
+                rb.AddForce(sideForce, 0, 0);
+            }
+
+            if (Input.GetKey("w"))
+            {
+                rb.AddForce(0, 0, forwardForce);
+            }
+
+            if (Input.GetKey("s"))
+            {
+                rb.AddForce(0, 0, -forwardForce);
+            }
+        }
+    }
+
+    public void pauseMovement(bool pause = true)
+    {
+        m_movementPaused = pause;
+    }
+
+    void FixedUpdate()
+    {
+        if (!m_movementPaused)
+        {
+            if (InWindZones > 0)
+            {
+                rb.AddForce(WindZone.GetComponent<WindArea>().Direction * WindZone.GetComponent<WindArea>().Force);
+            }
+
+            else
+            {
+                rb.AddForce(0, 0, 0);
+            }
+        }
+    }
+
+}
